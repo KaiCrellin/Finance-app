c711b2d4e12d4af3e0e66af99a7c39a5ef3d2cbc
KaiCrellin <kaicrellin@live.co.uk>
2025-10-31 17:03:19 +0000

    Add, Delete and get all items with name and price

--- finance-application-backend/index.js
+++ finance-application-backend/index.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -5,7 +6,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 
-
+// Express connection and cors middleware
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,42 +14,72 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+// Connection
 mongoose.connect(process.env.MONGO_URI, {})
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB Connection Error", err));
 
+// Connection 
 
+// Tables
+const ItemSchema = new mongoose.Schema({
+    Item: { type: String, requried: true },
+    price: { type: Number, required: true }
+
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+//Tables
+//API Interaction
 app.use(
     helmet.contentSecurityPolicy({
         directives: {
             defaultSrc: ["'self'"],
-            connectSrc: ["'self'", "http://localhost:5000"]
+            connectSrc: ["'self'", "http://localhost:5000"],
         },
     })
 );
 
-
-app.get('/db', async (req, res) => {
-    const states = ['disconnected', 'connected', 'connecting', 'disconnectiong'];
+app.get('/state', async (req, res) => {
+    const states = ['disconnected', 'Connected', 'Connecting', 'disconnecting'];
     const state = states[mongoose.connection.readyState] || 'unknown';
     res.json({ status: state })
 });
 
-app.get('/test', (req, res) => {
-    const test = ["test"];
-    res.json(test);
+
+app.post('/items', async (req, res) => {
+    const { name, price } = req.body
+    const item = new Item({ Item: name, price });
+    await item.save();
+    res.status(201).send(item);
 });
 
 
-app.get('/', async (req, res) => {
-    res.send('backend it running');
+app.get('/items', async (req, res) => {
+    const items = await Item.find();
+    res.send(items);
 });
 
+app.delete('/items/:id', async (req, res) => {
+    try {
+        const deletedItem = await Item.findByIdAndDelete(req.params.id);
+        if (!deletedItem) return res.status(404).send({ Message: 'Item is not found' });
+        res.send({ Message: 'Item Deleted Successfully', item: deletedItem });
+    } catch (error) {
+        res.status(500).send({ Message: 'Error deleted item', error });
+    }
+});
 
+// API Interaction
+
+// Terminal Debugging - Checking State and PORT
 app.listen(PORT, () => {
     console.log(`Server Running on PORT: ${PORT}`)
 });
 
 console.log('MongoDB URI:', process.env.MONGO_URI);
 console.log('MongoDB readyState:', mongoose.connection.readyState);
+
+// Terminal Debugging - Checking State and PORT
 
--- my-frontend/src/dbcheck.jsx
+++ my-frontend/src/dbcheck.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 function CheckDbStatus() {
   useEffect(() => {
-    fetch('http://localhost:5000/db')
+    fetch('http://localhost:5000/state')
       .then(res => res.json())
       .then(data => {
         console.log('MongoDB connection status:', data.status);
